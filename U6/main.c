a03a31de5ec70117adf44d9de87da8781f7d3ece
Imron <imron.gamidli@lt.hs-fulda.de>
2021-12-06 16:45:02 +0100

    minimal code

--- U6/main.c
+++ U6/main.c
@@ -4,26 +4,6 @@ long long seed;
 long long a = 96054250762914, b = 195253620256666;
 unsigned int z1 = 3512796597, z2 = 1516901688;
 
-unsigned int nextRandom(){
-    seed = (seed * a + b) & ((1L << 48) - 1);
-    return (unsigned int) (seed >> 16);
-}
-
-
-
-// function to convert decimal to binary
-void decToBinary(long n)
-{
-    // Size of an integer is assumed to be 32 bits
-    for (int i = 47; i >= 0; i--) {
-        int k = n >> i;
-        if (k & 1)
-            printf("1");
-        else
-            printf("0");
-    }
-}
-
 int main(int argc, char const *argv[])
 {
         //3512796597 << 16
